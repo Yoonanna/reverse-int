4d0e74f1aafd925e6ddb33b327af2c0c5afb3835
Yoonanna <Yanykovaanya@gmail.com>
2023-02-13 13:02:31 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let str = n.toString();
+    let res = str.split('').reverse().join('').toString();
+    return res;
 }
